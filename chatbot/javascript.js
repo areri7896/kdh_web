371bae6bdb369c5f77f8a472eb7e0e7b394f1c52
Areri Rodney Areri <arerirodney@gmail.com>
2023-03-20 20:03:13 +0300

    addition of testimonials

--- chatbot/javascript.js
+++ chatbot/javascript.js
@@ -1,22 +1,18 @@
 function talk(){
-var know = {
-"Who are you" : "Hello, Codewith_Newbie here ",
-"How are you" : "Good :)",
-"What can i do for you" : "Please Give us A Follow & Like.",
-"Your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy ",
-"ok" : "Thank You So Much ",
-"Bye" : "Okay! Will meet soon.."
-};
-var user = document.getElementById('userBox').value;
-document.getElementById('chatLog').innerHTML = user + "<br>";
-if (user in know) {
-document.getElementById('chatLog').innerHTML = know[user] + "<br>";
-}else{
-document.getElementById('chatLog').innerHTML = "Sorry,I didn't understand <br>";
-}
-}
-// const showAnswer = document.getElementById('chatLog'){
-//     function (talk) {
-//         document.style.display; { (toggle) => StyleSheet; }
-//     }
-// }
+    var know = {
+      "who are you" : "Hello, Codewith_Newbie here ",
+      "how are you" : "Good :)",
+      "what can i do for you" : "Please Give us A Follow & Like.",
+      "your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy ",
+      "ok" : "Thank You So Much ",
+      "bye" : "Okay! Will meet soon.."
+    };
+    var user = document.getElementById('userBox').value.toLowerCase(); // convert to lowercase
+    document.getElementById('chatLog').innerHTML = user + "<br>";
+    if (user in know) {
+      document.getElementById('chatLog').innerHTML += know[user] + "<br>"; // use += to append to the chat log
+    } else {
+      document.getElementById('chatLog').innerHTML += "Sorry, I didn't understand <br>"; // use += to append to the chat log
+    }
+  }
+  
